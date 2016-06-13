9e416547fe28413c2dac93da3212e8f132c4d1c4
Daniel O <danielparatodo@gmail.com>
2016-06-13 22:49:13 +0200

    add gitignore

--- src/main/java/rancheros/com/domain/PersonRepository.java
+++ src/main/java/rancheros/com/domain/PersonRepository.java
@@ -2,12 +2,16 @@ package rancheros.com.domain;
 
 import java.util.List;
 
+import org.springframework.data.repository.CrudRepository;
+
 /**
  * Created by Daniel on 11/06/2016.
  */
 public interface PersonRepository {
 
     List<Person> findAll();
+
     Person findById(String id);
+
     Person create(Person person);
 }
