86c5346a80e2d4669ff60a2bf74090465179d353
Daniel Ocaña Heredia <Daniel Ocaa Heredia>
2016-07-31 01:57:08 +0200

    Fix swagger.

--- src/main/java/rancheros/com/spring/configuration/SwaggerConfiguration.java
+++ src/main/java/rancheros/com/spring/configuration/SwaggerConfiguration.java
@@ -1,38 +1,50 @@
 package rancheros.com.spring.configuration;
 
-/**
- * Created by Daniel on 04/07/2016.
- */
-
+import com.fasterxml.classmate.TypeResolver;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
-import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+import javax.inject.Inject;
+
 @Configuration
 @EnableSwagger2
 public class SwaggerConfiguration {
-
     @Bean
-    public Docket newsApi() {
+    public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
-                .groupName("Rancheros")
                 .apiInfo(apiInfo())
                 .select()
-                .paths(PathSelectors.any())
-                .build();
+                .apis(RequestHandlerSelectors.basePackage("rancheros.com.spring.controllers"))
+                .build()
+                .pathMapping("/");
     }
+
+    @Inject
+    private TypeResolver typeResolver;
 
     private ApiInfo apiInfo() {
         return new ApiInfoBuilder()
-                .title("Spring REST Sample with Swagger")
-                .description("Spring REST Sample with Swagger")
+                .title("Overview")
+                .description("Lorem Ipsum is simply dummy text of the printing and typesetting industry." +
+                        " Lorem Ipsum has been the industry's standard dummy text ever since the 1500s," +
+                        " when an unknown printer took a galley of type and scrambled it to make a type specimen book. " +
+                        "It has survived not only five centuries, but also the leap into electronic typesetting, " +
+                        "remaining essentially unchanged." +
+                        " It was popularised in the 1960s with the release of Letraset sheets containing " +
+                        "Lorem Ipsum passages, and more recently with desktop publishing software like " +
+                        "Aldus PageMaker including versions of Lorem Ipsum." )
+                .termsOfServiceUrl("http://springfox.io")
+                .contact(new Contact("springfox","",""))
                 .license("Apache License Version 2.0")
-                .version("2.0")
+                .licenseUrl("https://github.com/springfox/springfox/blob/master/LICENSE")
+                .version("1.0")
                 .build();
     }
 }
