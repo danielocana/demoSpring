865bdc478053d2d71470a8714dc641dca5ec19f6
Daniel Ocaña Heredia <Daniel Ocaa Heredia>
2016-08-30 22:19:41 +0200

    add test controller.

--- src/test/java/rancheros/com/spring/controllers/PetControllerTest.java
+++ src/test/java/rancheros/com/spring/controllers/PetControllerTest.java
@@ -1,33 +1,32 @@
 package rancheros.com.spring.controllers;
 
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.http.MediaType;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 import rancheros.com.aplication.DemoApplicationTest;
-import rancheros.com.spring.DemoApplication;
-
-import javax.inject.Inject;
-
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+import javax.inject.Inject;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-@SpringApplicationConfiguration(classes = DemoApplication.class)
-@WebAppConfiguration
+@Ignore
 @RunWith(SpringJUnit4ClassRunner.class)
-@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:beforePersonTestRun.sql")
-public class PersonControllerTest {
+@SpringApplicationConfiguration(classes = DemoApplicationTest.class)
+@WebAppConfiguration
+@Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:beforePetTestRun.sql")
+public class PetControllerTest {
 
     private MockMvc mockMvc;
+
     @Inject
     private WebApplicationContext webApplicationContext;
 
@@ -37,33 +36,20 @@ public class PersonControllerTest {
     }
 
     @Test
-    public void findAllPersonsTest() throws Exception {
-        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/persons")
-                .contentType(MediaType.APPLICATION_JSON);
+    @Ignore
+    public void findAllPetsTest() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/pets");
 
         mockMvc.perform(requestBuilder)
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$").isArray());
+                .andExpect(status().isOk());
     }
 
     @Test
-    public void findById() throws Exception {
+    @Ignore
+    public void findByIdTest() throws Exception {
+        MockHttpServletRequestBuilder requestBuilder = MockMvcRequestBuilders.get("/pets/1");
 
+        mockMvc.perform(requestBuilder)
+                .andExpect(status().isOk());
     }
-
-    @Test
-    public void create() throws Exception {
-
-    }
-
-    @Test
-    public void update() throws Exception {
-
-    }
-
-    @Test
-    public void delete() throws Exception {
-
-    }
-
 }
---
+++ src/test/resources/beforePetTestRun.sql
@@ -0,0 +1,6 @@
+--Person--
+
+TRUNCATE TABLE pet CASCADE;
+
+INSERT INTO pet (id, name, type) VALUES
+  ('1', 'flyway-named', 'dino'), ('2', 'knock', 'lizard');
