870693853f80ac53a3b2121b19c08797918d79da
Daniel Ocaña Heredia <Daniel Ocaa Heredia>
2016-11-13 15:10:36 +0100

    fix

--- src/test/java/rancheros/com/spring/controllers/PetControllerTest.java
+++ src/test/java/rancheros/com/spring/controllers/PetControllerTest.java
@@ -7,6 +7,7 @@ import org.junit.runner.RunWith;
 import org.springframework.boot.test.SpringApplicationConfiguration;
 import org.springframework.test.context.jdbc.Sql;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -24,7 +25,7 @@ import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppC
 @Category(IntegrationTest.class)
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = DemoApplicationTest.class)
-//@WebAppConfiguration
+@WebAppConfiguration
 @Sql(executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD, scripts = "classpath:beforePetTestRun.sql")
 public class PetControllerTest {
 
