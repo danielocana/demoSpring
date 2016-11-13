9980ee9245be4c6352a07f0043f4a44230b18747
Daniel Ocaña Heredia <Daniel Ocaa Heredia>
2016-11-13 14:37:40 +0100

    save

--- src/test/java/rancheros/com/DemoApplicationTest.java
+++ src/test/java/rancheros/com/DemoApplicationTest.java
@@ -3,7 +3,6 @@ package rancheros.com;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Import;
-import rancheros.com.spring.DemoApplication;
 import rancheros.com.spring.configuration.PersonConfiguration;
 import rancheros.com.spring.configuration.PetConfiguration;
 
